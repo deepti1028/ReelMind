a1fd020dbe140647a706ef85127b961b914e7634
Deepti Jain <deepit.jain@pbhealth.com>
2026-05-01 16:01:29 +0530

    Initial Commit

---
+++ ReelMind/ReelMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelMindApp.swift
+//  ReelMind
+//
+//  Created by Deepti Jain on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ReelMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
