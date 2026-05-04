4f4f7af8e457560cc9d6c4f96296ecb0b62000dc
Deepti Jain <deepit.jain@pbhealth.com>
2026-05-04 22:52:00 +0530

    added fcm server key in render file

---
+++ Podfile
@@ -0,0 +1,23 @@
+platform :ios, '14.0'
+
+target 'ReelMind' do
+  use_frameworks!
+
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Messaging'
+
+end
+
+target 'URL Sharing module' do
+  use_frameworks!
+
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '14.0'
+      config.build_settings['ENABLE_USER_SCRIPT_SANDBOXING'] = 'NO'
+    end
+  end
+end
--- frontend/ReelMindApp.swift
+++ frontend/ReelMindApp.swift
@@ -6,9 +6,60 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseMessaging
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Configure Firebase
+        FirebaseApp.configure()
+
+        // Set up messaging delegate
+        Messaging.messaging().delegate = self
+
+        // Request user notification permissions
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
+
+        return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("FCM Token: \(fcmToken ?? "No token")")
+        // Send this token to your backend for storing user's FCM token
+        if let token = fcmToken {
+            UserDefaults.standard.set(token, forKey: "fcmToken")
+        }
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        print("Notification received in foreground: \(userInfo)")
+        completionHandler([.banner, .sound, .badge])
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        print("User tapped notification: \(userInfo)")
+        completionHandler()
+    }
+}
 
 @main
 struct ReelMindApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
