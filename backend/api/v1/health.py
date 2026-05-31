719534ae72f004386af7b9c768f4a8be9b2e8e19
Deepti Jain <deepit.jain@pbhealth.com>
2026-06-01 01:43:07 +0530

    added head in api health

--- backend/api/v1/health.py
+++ backend/api/v1/health.py
@@ -5,6 +5,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/health")
+@router.api_route("/health", methods=["GET", "HEAD"])
 async def health_check():
     return {"status": "ok"}
